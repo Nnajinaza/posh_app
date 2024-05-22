93e50eb272c6442d70711fb3c110fd7c9bfd6681
Nnajinaza <nnajinaza@gmail.com>
2024-05-22 17:42:55 +0100

    update navbar

--- E-commerce/src/component/navheader/Header.jsx
+++ E-commerce/src/component/navheader/Header.jsx
@@ -6,90 +6,75 @@ import { FaRegHeart } from "react-icons/fa";
 function Nav() {
   return (
     <>
-      <header className="bg-pink-300 border-bottom-black p-3 text-gray-500 text-base font-medium font flex justify-between">
+      <header className="bg-pink-100 border-bottom-black py-2 px-4 text-gray-500 text-sm font-medium flex justify-between">
         <div class="flex items-center"> 
           <p class="text-black">Free shipping over $1000 & Free returns</p>
         </div> 
         <div class="flex items-center"> 
-          <p class="text-black flex items-center  mr-2">
+          <p class="text-black flex items-center mr-2">
             <BsTelephoneFill /> {/* Tel : {" "} */}
             <a href="tel: +234814"> +234814</a>
           </p>
         </div> 
       </header>
-      <header className="bg-pink-300">
-        <div className="container-xxl">
-          <div className="row">
-            <div className="col-1">
-              <h2 className="title align-item-center justify-content-center ">
-                <NavLink to="/" className="text-black">POSH</NavLink>
+      <header className="bg-pink-100 items-center">
+        <div className="flex align-middle py-2 px-4 justify-between items-center">
+              <div className="flex align-middle items-center gap-4">
+              <h2 className="items-center justify-between ">
+                <NavLink to="/" className="text-black text-center text-lg font-semibold from-neutral-800 to-lime-700">POSH</NavLink>
               </h2>
-            </div>
-            <div className="col-3">
-              <div className="input-group mb-2">
                 <input
                   type="text"
                   placeholder="Search"
-                  className="form-control pl-2"
+                  className="form-control pl-2 h-6 align-middle justify-between rounded-md"
                   aria-label="Search"
                   aria-describedby="basic-addon2"
-                />
-                <span className="input-group-text bg-blue" id="basic-addon2">
+                  />
+                <span className="input-group-text mt-[-2px] h-3 ml-[-38px]" id="basic-addon2">
                   <BsSearch className="fs-6" />
                 </span>
-              </div>
-            </div>
-            <div className="col-5">
-              <div></div>
-            </div>
-            <div className="col-3 ">
-              <div className="middle-header-links d-flex align-item-left justify-content-between gap-10 ">
+                  </div>
+              <div className="middle-header-links flex align-item-left justify-content-between gap-5 ">
                 <div>
-                  <NavLink to="/wishlist" className="d-flex align-item-center gap-10 text-black">
+                  <NavLink to="/wishlist" className="flex items-center gap-1 text-black">
                     <FaRegHeart className="fs-4" />
-                    {/* <img src="/images/wishlist.svg"/> */}
                     <p>Wishlist</p>
                   </NavLink>
                 </div>
                 <div>
-                  <NavLink to="/cart" className="d-flex align-item-center gap-10 text-black">
-                    <BsCartFill fontSize={24} className="fs-3" />
-                    <div className="d-flex flex-column">
-                      <span className="badge bg-black">0</span>
-                    </div>
+                  <NavLink to="/cart" className="flex items-center gap-1 text-black">
+                    <BsCartFill fontSize={18} className="fs-3" />
+                    {/* <div className="flex flex-column">
+                      <span className="badge bg-white p-2 h-4 text-xs text-balance ">0</span>
+                    </div> */}
                     <p>Cart</p>
                   </NavLink>
                 </div>
                 <div>
-                  <NavLink to="/register" className="d-flex align-item-center gap-10 text-black">
+                  <NavLink to="/register" className="flex items-center gap-1 text-black">
                     <BsPersonCircle className="fs-3" />
                     <p>Profile</p>
                   </NavLink>
                 </div>
               </div>
-            </div>
-          </div>
         </div>
       </header>
-      <header className="bottom-header py-3">
-        <div className="container-xxl">
-          <div className="row">
-            <div className="col-12">
-              <div className="menu-button d-flex align-items-center gap-30">
+      <header className="bg-pink-300 justify-between">
+              <div className="menu-button flex items-center gap-30 justify-between py-2 px-4">
                 <div>
                   <div className="dropdown">
                     <button
-                      className="btn btn-secondary dropdown-toggle bg-transparent border-0 gap-15 d-flex align-items-center"
+                      className="btn btn-secondary dropdown-toggle bg-transparent border-0 gap-2 flex items-center"
                       type="button"
                       id="dropDownMenuButton1"
                       data-bs-toggle="dropdown"
                       aria-expanded="false"
                     >
-                      <img src="/images/menu.svg" />
+                      <img className="h-5" src="/images/menu.svg" />
                       {/* <BsFillMenuAppFill/> */}
                       <span className="mc-5 d-inline-block">Categories</span>
                     </button>
-                    <ul
+                    {/* <ul
                       className="dropdown-menu"
                       aria-labelledby="dropdownmenuButton1"
                     >
@@ -111,11 +96,11 @@ function Nav() {
                       <li>
                         <Link className="dropdown-item">Product D</Link>
                       </li>
-                    </ul>
+                    </ul> */}
                   </div>
                 </div>
                 <div className="menu-links">
-                  <div className="d-flex align-items-center gap-15">
+                  <div className="flex items-center gap-2 uppercase text-sm font-medium">
                     <NavLink to="/">Home</NavLink>
                     <NavLink to="/products">Our Store</NavLink>
                     <NavLink to="/blog">Blog</NavLink>
@@ -123,9 +108,6 @@ function Nav() {
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
-        </div>
       </header>
     </>
   );
